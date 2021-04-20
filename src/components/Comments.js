1f3211b4fcb787d357c645cdbece5036fa20fe5e
Maxim <shafeevms@gmail.com>
2021-04-20 17:32:25 +0300

    loader changed

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { getComments } from '../store/commentsActions';
 import { useSelector, useDispatch } from 'react-redux';
+import Loader from './Loader';
 
 const Comments = () => {
   const dispatch = useDispatch();
@@ -14,14 +15,17 @@ const Comments = () => {
     <>
       {
         isLoading
-          ? <div>Loading...........</div>
-          : data.map(comment => (
+          ? <Loader />
+          : null
+        }
+        {
+          data.map(comment => (
           <article className="comment" key={comment.id}>
             <h3 className="comment__name">{comment.name}:</h3>
             <p className="comment__text">{comment.comment}</p>
           </article>
         ))
-      }
+        }
     </>
   );
 };
---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <img className="loader__img" src="./img/loader.png"/>
+    </div>
+  );
+};
+
+export default Loader;
