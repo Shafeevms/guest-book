d41f7b32bd5deda472b9bfbd4a46d66fd36a97d5
Maxim <shafeevms@gmail.com>
2021-04-20 18:10:00 +0300

    loader fix1

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loader = () => {
   return (
     <div className="loader">
-      <img className="loader__img" src="./img/loader.png"/>
+      <img className="loader__img" src="../img/loader.png"/>
     </div>
   );
 };
